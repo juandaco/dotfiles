176745a9c5351aa10d20f062abd0c6fcc526a177
Juan D. Acosta <juandacorias@gmail.com>
2020-04-10 21:55:28 -0500

    Corrections.

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# shellcheck disable=1090
 
 # Link Git configuration file
 ln -vf gitconfig ~/.gitconfig
@@ -8,6 +9,25 @@ ln -vf tmux.conf ~/.tmux.conf
 
 # Make sure bin directory exists
 mkdir -p "$HOME/bin"
+
+# Install bash directories
+pushd "$(dirname "$0")/bash" >/dev/null || exit 1
+
+# ~/.pforile
+ln -vf ./profile "$HOME/.profile"
+
+# ~/.bash_profile
+ln -vf ./bash_profile "$HOME/.bash_profile"
+
+# ~/.bashrc
+ln -vf ./bashrc "$HOME/.bashrc"
+
+# ~/.bash_aliases
+ln -vf ./bash_aliases "$HOME/.bash_aliases"
+
+popd >/dev/null || exit 1
+
+source "$HOME/.bash_profile"
 
 # Run all installer scripts
 for file in ./installers/*; do
